a37936bb3588d927eea364af25cd898c3d433287
KongHaiAdam <kongha-wm19@student.tarc.edu.my>
2020-07-04 10:11:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.HelloConstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private int mCount = 0;
+private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+
+
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_massage,Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+
+    public void countUp(View view){
+        ++mCount;
+       if (mShowCount != null)
+           mShowCount.setText(Integer.toString(mCount));
+    if(mCount>0)
+    {
+        findViewById(R.id.button_zero).setBackgroundColor(Color.GREEN);
+    }
+        if(mCount % 2 == 0)
+            view.setBackgroundColor(Color.RED);
+        else
+            view.setBackgroundColor(Color.BLUE);
+
+    }
+    public void zero(View view){
+        if(mCount !=0)
+        {
+            view.setBackgroundColor(Color.GRAY);
+        }
+        mCount = 0;
+        mShowCount.setText(Integer.toString(0));
+
+    }
+}
